3bc9a5663a4a5d8063352b38efc51a3a651a54cb
nvbn <nvbn.rm@gmail.com>
2013-11-26 22:33:21 +0400

    Add subtitle loader from addic7ed

---
+++ series_list/loaders/subtitles.py
@@ -0,0 +1,44 @@
+from urllib import urlencode
+from BeautifulSoup import BeautifulSoup
+import requests
+
+
+class Addic7edLoader(object):
+    """Subtitle loader from addicted"""
+
+    def _get_url(self, name):
+        """Get url for name"""
+        return 'http://www.addic7ed.com/search.php?{}'.format(
+            urlencode({'search': name, 'Submit': 'Search'}),
+        )
+
+    def _fetch_search(self, name):
+        """Fetch search result"""
+        return requests.get(self._get_url(name)).content
+
+    def _get_episode_url(self, html):
+        """Get episode url from html"""
+        soup = BeautifulSoup(html)
+        relative = soup.find('table', {'class': 'tabel'}).find('tr')\
+            .findAll('td')[1].find('a')['href']
+        return 'http://www.addic7ed.com/{}'.format(relative)
+
+    def _fetch_episode(self, url):
+        """Fetch episode"""
+        return requests.get(url).content
+
+    def _parse_episode(self, html):
+        """Parse episode page"""
+        soup = BeautifulSoup(html)
+        relative = soup.find('a', {'class': 'buttonDownload'})['href']
+        return 'http://www.addic7ed.com{}'.format(relative)
+
+    def get_subtitle_url(self, name):
+        """Get subtitle url"""
+        search = self._fetch_search(name)
+        try:
+            episode_url = self._get_episode_url(search)
+        except AttributeError:
+            return None
+        episode = self._fetch_episode(episode_url)
+        return self._parse_episode(episode)
---
+++ tests/test_subtitles.py
@@ -0,0 +1,40 @@
+import sure
+from unittest import TestCase
+from mock import MagicMock
+from series_list.loaders.subtitles import Addic7edLoader
+from .base import get_fixture
+
+
+class Addic7edLoaderCase(TestCase):
+    """Subtitles loader case"""
+
+    def setUp(self):
+        self._mock_fetch()
+        self.loader = Addic7edLoader()
+
+    def _mock_fetch(self):
+        """Mock fetching data"""
+        self._orig_fetch_search = Addic7edLoader._fetch_search
+        self._orig_fetch_episode = Addic7edLoader._fetch_episode
+        Addic7edLoader._fetch_search = MagicMock(
+            return_value=get_fixture('addicted.html'),
+        )
+        Addic7edLoader._fetch_episode = MagicMock(
+            return_value=get_fixture('addicted_single.html'),
+        )
+
+    def tearDown(self):
+        Addic7edLoader._fetch_search = self._orig_fetch_search
+        Addic7edLoader._fetch_episode = self._orig_fetch_episode
+
+    def test_get_subtitle_url(self):
+        """Test get subtitle url"""
+        self.loader.get_subtitle_url('family guy').should.be\
+            .equal('http://www.addic7ed.com/original/71889/0')
+
+    def test_get_none_when_not_found(self):
+        """Tet get none when not found"""
+        Addic7edLoader._fetch_search = MagicMock(
+            return_value=get_fixture('addicted_none.html'),
+        )
+        self.loader.get_subtitle_url('family guy').should.be.none
