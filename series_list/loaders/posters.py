1d16111eb2b646b37f14166e524289e3b860c7c0
nvbn <nvbn.rm@gmail.com>
2013-11-26 20:30:24 +0400

    Add imdb poster loader

---
+++ series_list/loaders/posters.py
@@ -0,0 +1,32 @@
+from urllib import urlencode
+from BeautifulSoup import BeautifulSoup
+import requests
+
+
+class IMDBPosterLoader(object):
+    """IMDb poster loader"""
+    default_poster = 'http://ia.media-imdb.com/images/G/01/imdb/'\
+        'images/nopicture/32x44/film-3119741174._V379391527_.png'
+
+    def _get_url(self, name):
+        """Get url for fetching"""
+        return 'http://www.imdb.com/find?{}'.format(
+            urlencode({'q': name, 's': 'all'}),
+        )
+
+    def _fetch_html(self, name):
+        """Fetch html"""
+        return requests.get(self._get_url(name)).content
+
+    def _parse_html(self, html):
+        """Extract image from html"""
+        soup = BeautifulSoup(html)
+        return soup.find('td', {'class': 'primary_photo'}).find('img')['src']
+
+    def get_poster(self, name):
+        """Get poster"""
+        html = self._fetch_html(name)
+        try:
+            return self._parse_html(html)
+        except AttributeError:
+            return self.default_poster
---
+++ tests/test_posters_loader.py
@@ -0,0 +1,32 @@
+import sure
+from unittest import TestCase
+from mock import MagicMock
+from series_list.loaders.posters import IMDBPosterLoader
+from .base import get_fixture
+
+
+class IMDBPosterLoaderCase(TestCase):
+    """IMDb poster loader case"""
+
+    def setUp(self):
+        self._mock_fetch()
+        self.loader = IMDBPosterLoader()
+
+    def _mock_fetch(self):
+        """Mock fetching data"""
+        self._orig_fetch = IMDBPosterLoader._fetch_html
+        IMDBPosterLoader._fetch_html = MagicMock()
+
+    def test_get_poster(self):
+        """Test get poster"""
+        IMDBPosterLoader._fetch_html.return_value = get_fixture('imdb.html')
+        self.loader.get_poster('family gut').should.be\
+            .equal('http://ia.media-imdb.com/images/M/MV5BMTY4MjIwMjcxMF5BMl5'
+                   'BanBnXkFtZTYwMDA1NDg5._V1_SY44_CR0,0,32,44_.jpg')
+
+    def test_get_default_poster_if_not_exists(self):
+        """Test get default poster if not exists"""
+        IMDBPosterLoader._fetch_html.return_value =\
+            get_fixture('imdb_none.html')
+        self.loader.get_poster('family gut').should.be.\
+            equal(self.loader.default_poster)
