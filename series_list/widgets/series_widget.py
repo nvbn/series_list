23ebbe6eb61281f144afe7327f11b939e6530027
nvbn <nvbn.rm@gmail.com>
2013-11-26 19:29:35 +0400

    Add "add" and clear to series widget

--- series_list/widgets/series_widget.py
+++ series_list/widgets/series_widget.py
@@ -5,3 +5,17 @@ from ..interface.loader import WithUiMixin
 class SeriesWidget(WithUiMixin, QWidget):
     """Series widget"""
     ui = 'series'
+
+    @property
+    def series_layout(self):
+        """Series layout"""
+        return self.seriesContainer.layout()
+
+    def add_entry(self, entry):
+        """Add entry to series list"""
+        self.series_layout.addWidget(entry)
+
+    def clear(self):
+        """Clear series widget"""
+        for index in range(self.series_layout.count()):
+            self.series_layout.itemAt(index).widget().deleteLater()
