c2a55a6a5bd89434cc46b25c66af21c985a99560
nvbn <nvbn.rm@gmail.com>
2013-11-27 11:05:24 +0400

    Little increase speed with thread pool

--- series_list/workers/posters.py
+++ series_list/workers/posters.py
@@ -1,7 +1,20 @@
-from PySide.QtCore import Signal, QObject, Slot
+from PySide.QtCore import Signal, QObject, Slot, QThreadPool, QRunnable
 from ..models import SeriesEntry
 from ..utils import ticked
 from .base import BaseWorkerThread
+
+
+class PosterReceiver(QRunnable):
+    """Poster receiver"""
+
+    def __init__(self, episode, on_finish):
+        super(PosterReceiver, self).__init__()
+        self._on_finish = on_finish
+        self._episode = episode
+
+    def run(self):
+        self._episode.load_poster()
+        self._on_finish()
 
 
 class PosterWorker(QObject):
@@ -12,13 +25,17 @@ class PosterWorker(QObject):
     def __init__(self, *args, **kwargs):
         super(PosterWorker, self).__init__(*args, **kwargs)
         self.need_poster.connect(self._get_poster)
+        self.pool = QThreadPool(self)
+        self.pool.setMaxThreadCount(5)
 
     @Slot(SeriesEntry, int)
     @ticked
     def _get_poster(self, episode, tick):
         """Get poster for episode"""
-        episode.load_poster()
-        self.received.emit(episode, tick)
+        runnable = PosterReceiver(
+            episode, lambda: self.received.emit(episode, tick),
+        )
+        self.pool.start(runnable)
 
 
 class PosterWorkerThread(BaseWorkerThread):
--- series_list/workers/subtitles.py
+++ series_list/workers/subtitles.py
@@ -1,7 +1,20 @@
-from PySide.QtCore import Signal, QObject, Slot
+from PySide.QtCore import Signal, QObject, Slot, QRunnable, QThreadPool
 from ..models import SeriesEntry
 from ..utils import ticked
 from .base import BaseWorkerThread
+
+
+class SubtitleReceiver(QRunnable):
+    """Subtitle receiver"""
+
+    def __init__(self, episode, on_finish):
+        super(SubtitleReceiver, self).__init__()
+        self._on_finish = on_finish
+        self._episode = episode
+
+    def run(self):
+        self._episode.load_subtitle()
+        self._on_finish()
 
 
 class SubtitleWorker(QObject):
@@ -12,13 +25,17 @@ class SubtitleWorker(QObject):
     def __init__(self, *args, **kwargs):
         super(SubtitleWorker, self).__init__(*args, **kwargs)
         self.need_subtitle.connect(self._get_subtitle)
+        self.pool = QThreadPool(self)
+        self.pool.setMaxThreadCount(5)
 
     @Slot(SeriesEntry, int)
     @ticked
     def _get_subtitle(self, episode, tick):
         """Get subtitle for episode"""
-        episode.load_subtitle()
-        self.received.emit(episode, tick)
+        runnable = SubtitleReceiver(
+            episode, lambda: self.received.emit(episode, tick),
+        )
+        self.pool.start(runnable)
 
 
 class SubtitleWorkerThread(BaseWorkerThread):
