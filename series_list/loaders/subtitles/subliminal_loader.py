5d9ebbdcee2bde73c25681d3a2cdfebcad8bc742
nvbn <nvbn.rm@gmail.com>
2013-12-02 17:43:27 +0400

    Check subtitles existens with subtitleseeker for subliminal backend

--- series_list/loaders/subtitles/subliminal_loader.py
+++ series_list/loaders/subtitles/subliminal_loader.py
@@ -1,21 +1,65 @@
+import requests
+from BeautifulSoup import BeautifulSoup
 from babelfish import Language
 import subliminal
 from ...models import Subtitle
 from ...settings import config
 from .. import library
+from ..base import return_if_timeout
 from .base import SubtitlesLoader
 
 
 @library.subtitles
 class SubliminalLoader(SubtitlesLoader):
-    """Subliminal loader"""
+    """Subliminal loader with subtitleseeker checker"""
 
+    def _get_search_page_url(self, name):
+        """Get search page url"""
+        return u'http://www.subtitleseeker.com/search/TV_EPISODES/{}'.format(
+            name,
+        )
+
+    def _fetch_search_page(self, name):
+        """Fetch search page"""
+        return requests.get(
+            self._get_search_page_url(name), timeout=config.subtitle_timeout,
+        ).content
+
+    def _get_episode_url(self, html):
+        """Get episode url"""
+        soup = BeautifulSoup(html)
+        url = soup.find('div', {'class': 'boxRowsInner'})\
+            .find('a', {'class': 'red'})['href']
+        return u'{}English/'.format(url)
+
+    def _fetch_episode(self, url):
+        """Fetch episode page"""
+        return requests.get(url).content
+
+    def _check_subtitles_exists(self, html):
+        """Check subtitles exists"""
+        soup = BeautifulSoup(html)
+        if soup.find('div', {'class': 'boxRowsInner'}):
+            return True
+        else:
+            return False
+
+    @return_if_timeout(None)
     def get_subtitle_url(self, name):
         """Always return ok"""
-        return Subtitle(
-            name=name,
-            wait_for_file=True,
-        )
+        html = self._fetch_search_page(name)
+        try:
+            episode_url = self._get_episode_url(html)
+        except AttributeError:
+            return None
+        episode_html = self._fetch_episode(episode_url)
+        if self._check_subtitles_exists(episode_html):
+            return Subtitle(
+                name=name,
+                wait_for_file=True,
+            )
+        else:
+            return None
 
     def download(self, model):
         """Download subtitles using subliminal"""
