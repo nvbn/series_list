1b96ef148d9a9f60e319a7c4789fa1cfdcdb6f17
nvbn <nvbn.rm@gmail.com>
2014-01-08 23:40:30 +0400

    Add lazy_for_all decorator

--- series_list/utils.py
+++ series_list/utils.py
@@ -11,3 +11,14 @@ def ticked(fnc, *args, **kwargs):
         tick = args[-1]
     if tick == QApplication.instance().tick:
         return fnc(*args, **kwargs)
+
+
+@decorator
+def _lazy_for_all(fnc, self):
+    """Lazy property decorator for all cls instances"""
+    cls = type(self)
+    attr = '_lazy_result_of_{}_for_{}'.format(fnc.__name__, cls.__name__)
+    if not hasattr(cls, attr):
+        setattr(cls, attr, fnc(self))
+    return getattr(cls, attr)
+lazy_for_all = lambda fnc: property(_lazy_for_all(fnc))
