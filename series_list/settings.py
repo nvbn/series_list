17352c60447378c176d98295dada0b5a021ea5c2
nvbn <nvbn.rm@gmail.com>
2014-01-09 08:46:35 +0400

    Use descriptors in config

--- series_list/settings.py
+++ series_list/settings.py
@@ -1,46 +1,60 @@
 import json
 import os
+from .utils import lazy
 from . import const
 
 
+class Var(object):
+    """Descriptor for settings variable"""
+
+    def __init__(self, default):
+        self._default = default
+        self._instance = None
+
+    @lazy
+    def _name(self):
+        """Get name of variable"""
+        for attr, value in type(self._instance).__dict__.items():
+            if value == self:
+                return attr
+
+    def __get__(self, instance, owner):
+        """Get settings variable"""
+        self._instance = instance
+        return instance.get_settings_dict().get(self._name, self._default)
+
+    def __set__(self, instance, value):
+        """Set settings variable"""
+        self._instance = instance
+        settings = instance.get_settings_dict()
+        settings[self._name] = value
+        instance.save_settings_dict(settings)
+
+
 class Config(object):
-    """Config object"""
+    """Configuration object"""
+    download_path = Var(const.DOWNLOAD_PATH)
 
-    def _variable(name, default):
-        @property
-        def variable(self):
-            return self._get_settings_dict().get(name, default)
+    series_timeout = Var(const.SERIES_TIMEOUT)
+    subtitle_timeout = Var(const.SUBTITLE_TIMEOUT)
+    poster_timeout = Var(const.POSTER_TIMEOUT)
 
-        @variable.setter
-        def variable(self, value):
-            settings = self._get_settings_dict()
-            settings[name] = value
-            self._save_settings_dict(settings)
+    posters_loader = Var(const.POSTERS_LOADER)
+    series_loader = Var(const.SERIES_LOADER)
+    subtitles_loader = Var(const.SUBTITLES_LOADER)
 
-        return variable
+    preview_minimum = Var(const.PREVIEW_MINIMUM)
 
-    download_path = _variable('download_path', const.DOWNLOAD_PATH)
+    max_retry = Var(const.MAX_RETRY)
 
-    series_timeout = _variable('series_timeout', const.SERIES_TIMEOUT)
-    subtitle_timeout = _variable('subtitle_timeout', const.SUBTITLE_TIMEOUT)
-    poster_timeout = _variable('poster_timeout', const.POSTER_TIMEOUT)
-
-    posters_loader = _variable('posters_loader', const.POSTERS_LOADER)
-    series_loader = _variable('series_loader', const.SERIES_LOADER)
-    subtitles_loader = _variable('subtitles_loader', const.SUBTITLES_LOADER)
-
-    preview_minimum = _variable('preview_minimum', const.PREVIEW_MINIMUM)
-
-    max_retry = _variable('max_retry', const.MAX_RETRY)
-
-    def _get_settings_dict(self):
+    def get_settings_dict(self):
         """Get settings dict"""
         if not os.path.exists(const.SETTINGS_PATH):
             return {}
         with open(const.SETTINGS_PATH) as settings_file:
             return json.loads(settings_file.read())
 
-    def _save_settings_dict(self, settings):
+    def save_settings_dict(self, settings):
         """Save settings dict"""
         with open(const.SETTINGS_PATH, 'w') as settings_file:
             settings_file.write(json.dumps(settings))
