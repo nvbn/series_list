5bf70765ada2e6d869133894f06cc9108673f6ad
nvbn <nvbn.rm@gmail.com>
2014-01-16 11:56:07 +0400

    Add ability to serialise model

--- series_list/lib/models.py
+++ series_list/lib/models.py
@@ -4,3 +4,11 @@ class BaseModel(object):
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    @property
+    def as_dict(self):
+        """Model as dict"""
+        return {
+            key: value for key, value in self.__dict__.items()
+            if not key.startswith('_')
+        }
